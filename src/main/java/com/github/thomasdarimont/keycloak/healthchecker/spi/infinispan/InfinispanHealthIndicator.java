25370189d4e8744826778552607b85a34d3bd9b2
Matheus Baptista <matheus.baptista@superlogica.com>
2025-07-03 12:50:24 -0300

    remove cast

--- src/main/java/com/github/thomasdarimont/keycloak/healthchecker/spi/infinispan/InfinispanHealthIndicator.java
+++ src/main/java/com/github/thomasdarimont/keycloak/healthchecker/spi/infinispan/InfinispanHealthIndicator.java
@@ -62,10 +62,8 @@ public class InfinispanHealthIndicator extends AbstractHealthIndicator {
     protected EmbeddedCacheManager lookupCacheManager() {
         try {
             Object cacheManager = new InitialContext().lookup(jndiName);
-            if (!(cacheManager instanceof EmbeddedCacheManager)) {
-                throw new RuntimeException("O objeto JNDI encontrado não é um EmbeddedCacheManager");
-            }
-            return (EmbeddedCacheManager) cacheManager;
+            // return (EmbeddedCacheManager) cacheManager;
+            return cacheManager;
         } catch (NamingException e) {
             log.warnv("Could not find EmbeddedCacheManager with name: {0}", jndiName);
             throw new RuntimeException(e);
